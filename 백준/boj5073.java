98ac2350b34261187537755761ebb25e0365b359
judemin <judemin2087@naver.com>
2024-07-02 15:41:49 +0900

    [SOLVED] : boj5073

---
+++ 백준/boj5073.java
@@ -0,0 +1,38 @@
+
+// boj5073
+import java.io.*;
+import java.util.*;
+
+public class boj5073 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int[] arr = new int[3];
+        while (true) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            arr[0] = Integer.parseInt(st.nextToken());
+            arr[1] = Integer.parseInt(st.nextToken());
+            arr[2] = Integer.parseInt(st.nextToken());
+
+            if (arr[0] == 0 && arr[1] == 0 && arr[2] == 0)
+                break;
+
+            int max = 0;
+            int sum = 0;
+            for (int i = 0; i < 3; i++) {
+                if (arr[i] > max)
+                    max = arr[i];
+                sum += arr[i];
+            }
+
+            if ((sum - max) <= max)
+                System.out.println("Invalid");
+            else if (arr[0] == arr[1] && arr[1] == arr[2])
+                System.out.println("Equilateral");
+            else if (arr[0] == arr[1] || arr[1] == arr[2] || arr[2] == arr[0])
+                System.out.println("Isosceles");
+            else
+                System.out.println("Scalene");
+        }
+    }
+}
